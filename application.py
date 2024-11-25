aa361c4aa99167ab8754daa234d51782f66cabbd
Kortion97 <vincenzomarzano97@homail.it>
2024-11-25 18:59:56 +0100

    modifica application

--- application.py
+++ application.py
@@ -1,6 +1,8 @@
-frp, flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
-
-@app.route("/")
-def hello():
-  return "Hello World!"
+ 
+@app.route('/')
+def home():
+   return render_template('index.html')
+if __name__ == '__main__':
+   app.run()
